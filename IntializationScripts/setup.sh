6cc7b86bcd47603b7dbbd35e48fe902fb4107bda
Saksham Dhull <sakshamdhull442@gmail.com>
2019-03-18 01:56:39 +0530

    Converted virt commands to be called under sudo as the rights of virt-manager are correctly updated until system is restarted.

--- IntializationScripts/setup.sh
+++ IntializationScripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This command sets up the user disa_server with sudo privileges on a computer needed for the VMs to run. 
+# This command sets up the user disa_server with sudo privileges on a computer needed for the VMs to run.
 # The password of this will be asdf
 # Another test user will be created which will NOT have admin privileges and will be used to ssh into the VM
 # The ubuntu install iso should be present here
@@ -22,14 +22,14 @@ echo "disa_server:asdf" | sudo chpasswd
 echo "disa_user:asdf" | sudo chpasswd
 sudo usermod -a -G sudo disa_server
 sudo usermod -aG libvirtd disa_server
-virsh net-create createnetwork.xml
-virsh pool-define-as $vmName dir - - - - /home/disa_server/kvm-pool/
-virsh pool-build $vmName
-virsh pool-start $vmName
-virsh pool-autostart $vmName
+sudo virsh net-create createnetwork.xml
+sudo virsh pool-define-as $vmName dir - - - - /home/disa_server/kvm-pool/
+sudo virsh pool-build $vmName
+sudo virsh pool-start $vmName
+sudo virsh pool-autostart $vmName
 sudo qemu-img create -f raw /home/disa_server/kvm-pool/$vmName.qcow2 5G
 sudo rm -rf ~/.cache/virt-manager/
-virt-install --name=$vmName --ram 1024 --vcpu=1 -c ~/kvm-isos/ubuntu16-server-unattended-install.iso --os-type=linux --os-variant=generic --disk path=/home/disa_server/kvm-pool/$vmName.qcow2 --graphics spice --network bridge=virbr0
+sudo virt-install --name=$vmName --ram 1024 --vcpu=1 -c ~/kvm-isos/ubuntu16-server-unattended-install.iso --os-type=linux --os-variant=generic --disk path=/home/disa_server/kvm-pool/$vmName.qcow2 --graphics spice --network bridge=virbr0
 sudo chmod 755 /home/disa_server/kvm-pool
 
 # PROBLEMS::: ssh keys must be created
